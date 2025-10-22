8c155874c748928a5a1c09eb8e8c10a0afdfc97a
JoaoMaganin <joaovitormontemor138@gmail.com>
2025-10-22 12:01:19 -0300

    feat: Rate limiting no API Gateway (10 req/seg)

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -4,14 +4,31 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 1000,      // 1000 milissegundos = 1 segundo
+        limit: 10,      // 10 requisições
+      },
+    ]),
     AuthModule,
     TasksModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // 2. APLIQUE O "SEGURANÇA" (GUARD) GLOBALMENTE
+    // Isso faz com que CADA endpoint da sua API
+    // passe por este limitador de taxa.
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
